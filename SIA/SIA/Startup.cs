76112c2dd6a43845a0c60475fc8b33373d5c630b
pinhosilva <rafaelpinho01@gmail.com>
2015-11-01 21:18:12 -0300

    Projeto Inicial estrutura MVC

---
+++ SIA/SIA/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SIA.Startup))]
+namespace SIA
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
